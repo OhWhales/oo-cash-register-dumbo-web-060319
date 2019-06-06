2bdc75807b9350e1d3c6321a6086e834e89c4351
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-06-06 21:25:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,6 +12,7 @@ class CashRegister
   def add_item(title, price, quantity = 0 )
     if quantity == 0 
       @total = @total + price 
+      last_transaction = price
       @items.push(title)
     else 
       wait = price * quantity 
